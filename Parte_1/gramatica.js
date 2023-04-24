3cc98727396165139df9dc01354209ee3e745900
Joaquinnl22 <joaquin.neira@mail.udp.cl>
2023-04-23 21:38:38 -0400

    first

---
+++ Parte_1/gramatica.js
@@ -0,0 +1,18 @@
+//UpperCamelCase : Class
+class SerHumano {
+    constructor(nombre, genero) {
+      this.nombre = nombre;
+      this.genero = genero;
+    }
+  
+    miNombreEs() {
+      return `Mi nombre es ${this.nombre}`;
+    }
+  }
+
+// lowerCamelCase : Objeto
+
+const unObjeto = {
+    nombre: "Jonathan",
+    email: "jonmircha@gmail.com",
+  };
